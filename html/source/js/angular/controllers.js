991cf816bbcc4d8f73a814a33d2f235cd27564f3
Vladislav Nurov <vladislavnurov@ya.ru>
2016-02-08 00:50:23 +0500

    заполнил раздел абитуры

--- html/source/js/angular/controllers.js
+++ html/source/js/angular/controllers.js
@@ -1,9 +1,8 @@
-;
-'use strict';
-var ifsusuApp = angular.module('ifsusuApp', []);
 
-ifsusuApp.controller('postupashaList', ['postupashaList', function($scope){
-	
+var obuchenie = angular.module('obuchenie', []);
 
-
-}]);
+obuchenie.controller('napr', function($scope) {
+	$scope.obuchenieMenu = '123231'; 
+	$scope.content = '032123100';
+	$scope.k = '1';
+});
